cdee95aba2b88db98ece62b0264968a1153063cd
luism7171 <luis.martinez23@g.austincc.edu>
2020-10-03 04:02:17 +0000

    classwork 4 the looping assignment I wrote down the test case u believe will work my test cases don't want to run for some reasson will be going to office hours

--- src/classwork/04_assign/loops.cpp
+++ src/classwork/04_assign/loops.cpp
@@ -1,1 +1,15 @@
-//cpp
+#include "loops.h"
+int factorial(int num1)
+{
+  
+  int acumulator = 1,
+      total = 1;//sets the accumulator and total to 1
+//the loop does all the math until the acumulator equals the num entered
+  while (acumulator <= num1){
+    total = total * acumulator; 
+    acumulator++;
+   
+
+  }
+  return total;
+}
--- src/classwork/04_assign/main.cpp
+++ src/classwork/04_assign/main.cpp
@@ -1,15 +1,30 @@
-//write includes statements
+#include "loops.h"
+#include <iostream>
 
-//write using statements for cin and cout
+using std::cout; using std::cin;
+
+int main (){
+  int num1,total; // creates a variable for total and num1
+  char choice; // creates a variable for the leeter that decides to run loop again
+  choice = 'y';
 
 
-/*
-Use a do while loop to prompt the user for 
-a number, call the factorial function, and display the number's
-factorial.  Also, loop continues as long as user wants to.
-*/
-int main() 
-{
+  while (choice == 'y' || choice == 'Y')//this loop iterates as long as y or Y is selected
+  {
+    do// this loop ask the user for a number and makes sure its a valid number
+    {
+      //promts user for number
+      cout<<"Enter a number in the range of 1 trough 20 \n";
+      cin>>num1;
 
-	return 0;
+
+    }while(num1 < 1 || num1 > 20);
+    total = factorial(num1);//calls factorial function and sets it up in total veriable 
+    cout<<"the factorial total of "<<num1<<" is "<<total<<'\n';//displays results
+   
+    cout<<"would u like to run the program again\n";
+    cin>>choice;//ask user if we would like to run the loop again
+  }
+
+  return 0;
 }
